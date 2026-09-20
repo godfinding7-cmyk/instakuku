e68a9d52f335c0c97f8e9833258e76eef10448ef
godfinding7-cmyk <321503879+godfinding7-cmyk@users.noreply.github.com>
2026-09-20 14:06:36 +0530

    Delete assets directory

--- assets/site.js
+++
@@ -1,3 +0,0 @@
-const menuBtn = document.querySelector('[data-menu]');
-const navLinks = document.querySelector('[data-navlinks]');
-if (menuBtn && navLinks) menuBtn.addEventListener('click', () => navLinks.classList.toggle('open'));
